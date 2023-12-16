a73a34fcc2e9dea37c2e6b7565281f7e2adbba89
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-12-16 18:16:27 -0300

    Add files via upload

---
+++ Zoro.java
@@ -0,0 +1,6 @@
+// Classe derivada para Guerreiro (exemplo de herança)
+class Zoro extends Personagem {
+    public Zoro(String nome, int vida, Arma arma, Poder poder) {
+        super(nome, vida, arma, poder);
+    }
+}

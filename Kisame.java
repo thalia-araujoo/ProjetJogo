e37b553c77a1f9f8d53056e1ad493f825e26cd59
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-12-16 18:14:18 -0300

    Add files via upload

---
+++ Kisame.java
@@ -0,0 +1,8 @@
+// Classe derivada para Kisame
+class Kisame extends Personagem {
+    public Kisame(String nome, int vida, Arma arma, Poder poder) {
+        super(nome, vida, arma, poder);
+    }
+
+   
+}
